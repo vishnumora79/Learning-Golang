f2054cae898107ff9153fcb3c512773e0d20c11f
vishnu mora <moravishnu2005@gmail.com>
2025-09-13 07:29:28 +0530

    feat: Quering the database from Golang using database/sql package

--- data-access/main.go
+++ data-access/main.go
@@ -12,6 +12,13 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID int64
+	Title string
+	Artist string 
+	Price float32 
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -38,5 +45,37 @@ func main() {
 	}
 
 	fmt.Println("Connected to PostgreSQL!")
+
+
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+    	log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
 }
 
+
+func albumsByArtist(name string) ([]Album, error){
+	var albums []Album 
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = $1", name)
+
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album 
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
+}
